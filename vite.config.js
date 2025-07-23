1b504a4bd2d79efe0aa2ea9eedb8ecd537b8b71f
Jonathon Tech <59492@icf.com>
2025-07-23 14:55:43 -0500

    WIP AI Production Readiness

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,12 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { resolve } from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   base: "./",
   optimizeDeps: {
     exclude: ["lucide-react"],
-  },
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
-      "@/packages": resolve(__dirname, "./packages"),
-    },
   },
 });
